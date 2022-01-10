9810ce745251aa6945d89060c6c89f8962933d64
Enahoro123 <uvieeu@gmail.com>
2022-01-11 00:55:46 +0100

    Changed delay time on animations

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
 ScrollReveal().reveal('#welcome', { delay: 500 },{ reset: true });
-
+ScrollReveal().reveal('#icon', { delay: 1000 },{ reset: true });
 ScrollReveal().reveal('#message', { delay: 1800 },{ reset: true });
+ScrollReveal().reveal('.btn', { delay: 2300 },{ reset: true });
